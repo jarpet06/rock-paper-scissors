e1a54fe6761f5613816d47155736a65b7ac4ca20
Jared Peter <jaredpeter06@icloud.com>
2022-08-13 19:27:30 -0300

    Minor changes to playRound function and added game function that runs the game 5 times and gets the winner

--- script.js
+++ script.js
@@ -4,23 +4,57 @@ const getComputerChoice = () => {
     return choices[Math.floor(Math.random() * 3)]
 };
 
-function playRound(playerSelection, computerSelection) {
+const playRound = (playerSelection, computerSelection) => {
     playerSelection = playerSelection.toLowerCase();
-    const loseMsg = "You Lose! " + playerSelection + " beats" + computerSelection;
-    const winMsg = "You Win! " + playerSelection + " beats " + computerSelection;
+
+    if (playerSelection.charAt(0) === 'r') {
+        playerSelection = 'rock';
+    } else if (playerSelection.charAt(0) === 'p') {
+        playerSelection = 'paper';
+    } else if (playerSelection.charAt(0) === 's') {
+        playerSelection = 'scissors';
+    }
 
     if (playerSelection === computerSelection) {
         return "Tie!";
     } else {
+        const loseMsg = "You Lose! " + playerSelection + " loses to " + computerSelection;
+        const winMsg = "You Win! " + playerSelection + " beats " + computerSelection;
+
         if (playerSelection === 'rock') {
-            if (computerSelection === 'scissors') return winMsg;
-            return loseMsg;
+            if (computerSelection === 'scissors') return { winMsg };
+            return { loseMsg };
         } else if (playerSelection === 'paper') {
-            if (computerSelection === 'rock') return winMsg;
-            return loseMsg;
+            if (computerSelection === 'rock') return { winMsg };
+            return { loseMsg };
         } else if (playerSelection === 'scissors') {
-            if (computerSelection === 'paper') return winMsg;
-            return loseMsg;
+            if (computerSelection === 'paper') return { winMsg };
+            return { loseMsg };
         }
     }
 }
+
+const game = () => {
+    let userScore = 0;
+    let computerScore = 0;
+
+    for (let i = 0; i < 5; i++) {
+        const userSelection = prompt('Rock paper scissors. 5 round match');
+
+        const round = playRound(userSelection, getComputerChoice());
+
+        if (round.winMsg) {
+            alert(round.winMsg);
+            userScore++;
+        } else if (round.loseMsg) {
+            alert(round.loseMsg);
+            computerScore++;
+        } else {
+            alert(round);
+        }
+    }
+
+    alert(`Your score: ${userScore}\nComputer score: ${computerScore}`)
+}
+
+game()
