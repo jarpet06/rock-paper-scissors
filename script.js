79d9433b24abda9ba9bd58703cbe266e902b16b3
Jared Peter <jaredpeter06@icloud.com>
2022-08-13 19:01:05 -0300

    Added playRound function

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
-const getComputerChoice = () => {
-    const random = Math.floor(Math.random() * 6);
+const choices = ['rock', 'paper', 'scissors'];
 
-    if (random <= 2) {
-        return 'rock';
-    } else if (random <= 4) {
-        return 'paper';
-    } else if (random <= 6) {
-        return 'scissors';
-    }
+const getComputerChoice = () => {
+    return choices[Math.floor(Math.random() * 3)]
 };
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    const loseMsg = "You Lose! " + playerSelection + " beats" + computerSelection;
+    const winMsg = "You Win! " + playerSelection + " beats " + computerSelection;
+
+    if (playerSelection === computerSelection) {
+        return "Tie!";
+    } else {
+        if (playerSelection === 'rock') {
+            if (computerSelection === 'scissors') return winMsg;
+            return loseMsg;
+        } else if (playerSelection === 'paper') {
+            if (computerSelection === 'rock') return winMsg;
+            return loseMsg;
+        } else if (playerSelection === 'scissors') {
+            if (computerSelection === 'paper') return winMsg;
+            return loseMsg;
+        }
+    }
+}
